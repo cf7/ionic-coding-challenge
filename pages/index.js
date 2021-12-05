7b263c79f504194bc0764f5932fe9e90eb836fd4
cf7 <cf7@users.noreply.github.com>
2021-12-05 00:24:40 -0800

    header section

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Head from 'next/head'
 import Feature from './components/Feature.js';
+// import Navbar from './components/Navbar.js';
 
 export default function Home() {
   return (
@@ -13,9 +15,42 @@ export default function Home() {
       </Head>
 
       <main>
-        <Row>
-          Header Here
+
+        <Row className="header">
+          <Navbar>
+            <Navbar.Brand href="#home">
+              <img
+                src="/assets/logo.png"
+                width="40"
+                height="32"
+                // className="d-inline-block align-top"
+                alt="Ionic Challenge Logo"
+              />
+            </Navbar.Brand>
+            <Nav.Link>
+              Product
+            </Nav.Link>
+            <Nav.Link>
+              Features
+            </Nav.Link>
+          </Navbar>
+
+          <div className="copy">
+            <h2>Your best work. Done together.</h2>
+            <p>
+              Build better a business, faster. Start sharing your work across your company—in realtime.
+            </p>
+            <button>TRY IT FREE</button>
+            <button>DOWNLOAD</button>
+          </div>
+
+          <img
+            src="/assets/image_stock.png"
+            width="620"
+            height="634"
+          />
         </Row>
+
         <Row className="feature-section">
           <Col>
             <Row className="feature-section-header">
@@ -43,6 +78,7 @@ export default function Home() {
             </Row>
           </Col>
         </Row>
+
       </main>
     </div>
   );
