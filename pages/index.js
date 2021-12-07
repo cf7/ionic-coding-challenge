df5e4291011be4294930f0aa5af14c98d62d4766
cf7 <cf7@users.noreply.github.com>
2021-12-07 02:05:04 -0800

    footer links

--- pages/index.js
+++ pages/index.js
@@ -30,10 +30,22 @@ export default function Home() {
       header: "Do your best work",
       content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione."
     },
+    {
+      imgURL: "/assets/icon-f3.png",
+      header: "Avoid costly revisions",
+      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione."
+    },
+    {
+      imgURL: "/assets/icon-f4.png",
+      header: "Everything in one place",
+      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione."
+    },
   ];
   const footerLinks = {
-    "Solutions": ["Screen Sharing", "Marketing", "Commerce" ], 
-    "Company": ["About", "Blog", "Jobs"], 
+    "Solutions": ["Screen Sharing", "Marketing", "Commerce"],
+    "Resources": ["Pricing", "Documentation", "Guides", "API Status"],
+    "Company": ["About", "Blog", "Jobs", "Press"],
+    "Help": ["Support", "Training", "Tutorials"]
   };
   const logos = [ "/assets/logo-facebook1.png", "/assets/logo-instagram1.png", "/assets/logo-twitter1.png" ];
 
