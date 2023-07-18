815fd904040659aa128450993fbf4f9e1237b2d3
c <c@cs-macbook-air.lan>
2023-07-17 22:47:26 -0700

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "bootstrap/scss/bootstrap.scss";
 import "./scss/app.scss";
+import React from "react";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
