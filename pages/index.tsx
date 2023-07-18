808a65c61e4e0d36d5115ac4f3e3fb6b7198bbf3
c <c@cs-macbook-air.lan>
2023-07-17 22:59:58 -0700

    move components to views dir

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { Container } from "react-bootstrap";
 import React from "react";
-import { Header, FeatureSection, CTA, Footer } from "./components";
+import { Header, FeatureSection, CTA, Footer } from "../views/components";
 import { navLinks, features, footerLinks, logos } from "../lib/constants";
 
 const Index = ({}) => {
